a450231a30a453f59f202beedda7cedafd71ba15
Aiden X <caichao.xu@gmail.com>
2020-08-23 00:51:38 +0800

    feat(cli): add interactive cli lib

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 extern crate redis_cli;
 
 use redis_cli::RedisClient;
+use shrust::{Shell, ShellIO};
 
 fn main() {
-    let mut cli = RedisClient::new("127.0.0.1:6379");
-    let res = cli.ping();
-    println!("{:?}", res);
-    cli.set("xcc", "100000000000");
-    let d = cli.get("xcc");
-    println!("{:?}", d);
-    // let h = thread::spawn(move || {
-    //     let mut data = [0 as u8; 6]; // using 6 byte buffer
-    //     s.read(&mut data).unwrap();
-    //     println!("{:?}", data)
-    // });
-    // h.join().unwrap();
+    let cli = RedisClient::new("127.0.0.1:6379");
 
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    // for s in listener.incoming() {
-    //     let mut s = s.expect("failed");
-    //     let mut buf = [0; 20];
-    //     s.read(&mut buf).unwrap();
-    //     println!("{:?}", String::from_utf8_lossy(&mut buf))
-    // }
+    let mut shell = Shell::new(cli);
+    shell.new_command_noargs("ping", "Redis ping command", |_, cli| {
+        let res = cli.ping();
+        println!("{:?}", res);
+        Ok(())
+    });
+
+    shell.new_command("set", "Redis set command", 2, |_, cli, s| {
+        cli.set(s[0], s[1]);
+        Ok(())
+    });
+
+    shell.new_command("get", "Redis get command", 1, |_, cli, s| {
+        let res = cli.get(s[0]);
+        println!("{:?}", res);
+        Ok(())
+    });
+
+    shell.run_loop(&mut ShellIO::default());
 }
